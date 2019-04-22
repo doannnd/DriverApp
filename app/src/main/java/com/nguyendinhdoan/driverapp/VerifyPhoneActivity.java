9086d2c4368fe739787a1736b8918d850880bf0e
doannd <nguyendinhdoanjdk@gmail.com>
2019-04-22 15:09:11 +0700

    building ui of verify phone screen

--- app/src/main/java/com/nguyendinhdoan/driverapp/VerifyPhoneActivity.java
+++ app/src/main/java/com/nguyendinhdoan/driverapp/VerifyPhoneActivity.java
@@ -1,13 +1,64 @@
 package com.nguyendinhdoan.driverapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class VerifyPhoneActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    public static Intent start(Context context) {
+        return new Intent(context, VerifyPhoneActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_phone);
+
+        initViews();
+        setupUI();
+    }
+
+    private void initViews() {
+        toolbar = findViewById(R.id.toolbar);
+    }
+
+    private void setupUI() {
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        backLoginScreen();
+    }
+
+    private void backLoginScreen() {
+        Intent intentLogin = LoginActivity.start(this);
+        startActivity(intentLogin);
+        finish();
     }
 }
