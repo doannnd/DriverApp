344f67ceaa26f72263d5db96c57fce2060e95e0f
doannd <nguyendinhdoanjdk@gmail.com>
2019-05-05 23:39:11 +0700

    design driver home layout

--- app/src/main/java/com/nguyendinhdoan/driverapp/activity/DriverActivity.java
+++ app/src/main/java/com/nguyendinhdoan/driverapp/activity/DriverActivity.java
@@ -734,21 +734,22 @@ public class DriverActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_tools) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch (item.getItemId()) {
+            case R.id.nav_trip_history: {
+                break;
+            }
+            case R.id.nav_way_bill: {
+                break;
+            }
+            case R.id.nav_help: {
+                break;
+            }
+            case R.id.nav_settings: {
+                break;
+            }
+            case R.id.nav_sign_out: {
+                break;
+            }
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
